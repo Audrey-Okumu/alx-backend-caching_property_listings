936653304bbfc0558fec5b5a93b9df4bab2a7405
Audrey <audreyokumu@gmail.com>
2025-11-02 20:42:32 +0300

    adding the property path

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
 from .models import Property
 
 # Cache this view for 15 minutes (60 * 15 seconds)
@@ -10,5 +12,7 @@ def property_list(request):
     properties = Property.objects.all().values(
         'id', 'title', 'description', 'price', 'location', 'created_at'
     )
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({
+        "data": properties
+    })
 
