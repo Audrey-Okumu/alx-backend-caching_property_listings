99ce5eb36fefe6bad4bb93766b34adc841cd61ec
Audrey <audreyokumu@gmail.com>
2025-11-02 20:49:16 +0300

    Low-Level Caching for Property Queryset

---
+++ properties/utils.py
@@ -0,0 +1,18 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    """
+    Retrieve all Property objects from cache if available.
+    Otherwise, fetch from the database and cache for 1 hour.
+    """
+    properties = cache.get('all_properties')
+
+    if not properties:
+        print("Cache miss: Fetching from DB...")
+        properties = list(Property.objects.values())
+        cache.set('all_properties', properties, 3600)  # cache for 1 hour (3600 seconds)
+    else:
+        print("Cache hit: Retrieved from Redis")
+
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-from django.views.decorators.csrf import csrf_exempt
-from .models import Property
+from .utils import get_all_properties
 
 # Cache this view for 15 minutes (60 * 15 seconds)
 @cache_page(60 * 15)
 def property_list(request):
     """Return all property listings as JSON (cached in Redis for 15 minutes)."""
-    properties = Property.objects.all().values(
-        'id', 'title', 'description', 'price', 'location', 'created_at'
-    )
+    properties = get_all_properties()
     return JsonResponse({
         "data": properties
     })
